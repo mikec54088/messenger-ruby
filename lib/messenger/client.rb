5e930ce8222c6802e81d3940300a18b6c027f63f
Mike Chen <mikechen@MacBook-Pro-8.local>
2016-06-12 18:24:47 -0700

    Allow calls to override page access token

--- lib/messenger/client.rb
+++ lib/messenger/client.rb
@@ -1,15 +1,15 @@
 module Messenger
   class Client
-    def self.get_user_profile(user_id)
+    def self.get_user_profile(user_id, access_token = nil)
       JSON.parse(RestClient.get(
         "https://graph.facebook.com/v2.6/#{user_id}?fields=first_name,last_name,profile_pic \
-        &access_token=#{Messenger.config.page_access_token}",
+        &access_token=#{access_token || Messenger.config.page_access_token}",
       ))
     end
 
-    def self.send(data)
+    def self.send(data, access_token = nil)
       RestClient.post(
-        "https://graph.facebook.com/v2.6/me/messages?access_token=#{Messenger.config.page_access_token}",
+        "https://graph.facebook.com/v2.6/me/messages?access_token=#{access_token || Messenger.config.page_access_token}",
         data.build.to_json,
         content_type: :json
       )
